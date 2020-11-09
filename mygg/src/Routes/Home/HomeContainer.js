6c9c7f78d5d1660759a9ca6bd2ea290d281320ec
이희제 <holim1226@gmail.com>
2020-11-09 16:40:50 +0900

    base

---
+++ mygg/src/Routes/Home/HomeContainer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import HomePresenster from "./HomePresenter";
+import styled from "styled-components";
+
+const HomeContainer = () => {
+    return <HomePresenster />;
+};
+
+export default HomeContainer;
