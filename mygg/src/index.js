284d75f8da0ff15e651b64643950e001806c0f91
chkim116 <dudnqnfqlc@naver.com>
2020-11-10 18:58:55 +0900

    redux initial

--- mygg/src/index.js
+++ mygg/src/index.js
@@ -2,4 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import { createLogger } from "redux-logger";
+import rootReducer from "./modules";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const logger = createLogger();
+const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(logger))
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
