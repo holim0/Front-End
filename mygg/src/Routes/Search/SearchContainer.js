7157a55a7a7ba3150d570ddfa172d5801024f591
chkim116 <dudnqnfqlc@naver.com>
2020-11-19 16:14:52 +0900

    login axios sample

--- mygg/src/Routes/Search/SearchContainer.js
+++ mygg/src/Routes/Search/SearchContainer.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import SearchPresenter from "./SearchPresenter";
+import { useSelector, useDispatch } from "react-redux";
+import { searchRequest } from "modules/header";
 
 const SearchContainer = () => {
+    const { search } = useLocation();
+    const searchText = search.split("=")[1];
+    const { isSearchDone } = useSelector((state) => state.header);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (!isSearchDone) {
+            dispatch(searchRequest(searchText));
+        }
+    }, [dispatch, searchText, isSearchDone]);
     return <SearchPresenter />;
 };
 
